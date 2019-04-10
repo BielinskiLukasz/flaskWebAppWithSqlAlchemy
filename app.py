6f172a57b7bbebf718e4e9e74b2bcd45842ff0f5
Łukasz Paweł Bieliński <l.p.bielinski@gmail.com>
2019-04-10 21:25:33 +0200

    Refactor

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import models
 from models import Base
 
 DATABASE_URL = os.environ['DATABASE_URL']
-
 engine = create_engine(DATABASE_URL)
 
 db_session = scoped_session(
@@ -21,7 +20,7 @@ app = Flask(__name__)
 
 
 class InvalidUsage(Exception):
-    status_code = 400
+    status_code = 404
 
     def __init__(self, error, status_code=None, payload=None):
         super().__init__(self)
@@ -48,67 +47,6 @@ def shutdown_session(exception=None):
     db_session.remove()
 
 
-@app.route("/artists", methods=["POST"])
-def artists():
-    if request.method == "POST":
-        return post_artists()
-    abort(400)
-
-
-def post_artists():
-    data = request.json
-    new_name = data.get("name")
-    if new_name is None:
-        abort(400)
-
-    try:
-        art = models.Artist(name=new_name)
-        db_session.add(art)
-        db_session.commit()
-
-        artist = db_session.query(models.Artist).filter(models.Artist.name == new_name).with_for_update().one()
-        result_dict = artist.__dict__
-        print(result_dict)
-
-        del result_dict['_sa_instance_state']
-        dic = list(result_dict.keys())
-        for di in dic:
-            result_dict[di] = str(result_dict[di])
-
-        return jsonify(result_dict)
-    except:
-        abort(400)
-
-
-@app.route("/count_songs")
-def count_songs():
-    a = request.args
-    if 'artist' in a:
-        art = str(a['artist'])
-        art = art.split(",")
-
-    else:
-        abort(400)
-    try:
-        result_dict = {}
-        songs = (
-            db_session.query(models.Artist.name, func.count(models.Track.name))
-                .join(models.Track.album)
-                .join(models.Album.artist)
-                .filter(models.Artist.name.in_(art))
-                .group_by(models.Artist.name)
-        )
-        if len(songs.all()) == 0:
-            abort(400)
-
-        for u in songs.all():
-            result_dict[u[0]] = u[1]
-
-        return jsonify(result_dict)
-    except:
-        abort(400)
-
-
 @app.route("/longest_tracks")
 def longest_tracks():
     tracks = db_session.query(models.Track).order_by(models.Track.milliseconds.desc()).limit(10)
@@ -130,7 +68,7 @@ def longest_tracks_by_artist():
     if 'artist' in a:
         art = a['artist']
     else:
-        abort(400)
+        abort(404)
 
     try:
         tracks = db_session.query(models.Track).join(models.Track.album).join(models.Album.artist).filter(
@@ -145,12 +83,73 @@ def longest_tracks_by_artist():
                 i[di] = str(i[di])
 
         if len(result_dict) == 0:
-            abort(400)
+            abort(404)
 
     except:
-        abort(400)
+        abort(404)
 
     return jsonify(result_dict)
+
+
+@app.route("/artists", methods=["POST"])
+def artists():
+    if request.method == "POST":
+        return post_artists()
+    abort(404)
+
+
+def post_artists():
+    data = request.json
+    new_name = data.get("name")
+    if new_name is None:
+        abort(404)
+
+    try:
+        art = models.Artist(name=new_name)
+        db_session.add(art)
+        db_session.commit()
+
+        artist = db_session.query(models.Artist).filter(models.Artist.name == new_name).with_for_update().one()
+        result_dict = artist.__dict__
+        print(result_dict)
+
+        del result_dict['_sa_instance_state']
+        dic = list(result_dict.keys())
+        for di in dic:
+            result_dict[di] = str(result_dict[di])
+
+        return jsonify(result_dict)
+    except:
+        abort(404)
+
+
+@app.route("/count_songs")
+def count_songs():
+    a = request.args
+    if 'artist' in a:
+        art = str(a['artist'])
+        art = art.split(",")
+
+    else:
+        abort(404)
+    try:
+        result_dict = {}
+        songs = (
+            db_session.query(models.Artist.name, func.count(models.Track.name))
+                .join(models.Track.album)
+                .join(models.Album.artist)
+                .filter(models.Artist.name.in_(art))
+                .group_by(models.Artist.name)
+        )
+        if len(songs.all()) == 0:
+            abort(404)
+
+        for u in songs.all():
+            result_dict[u[0]] = u[1]
+
+        return jsonify(result_dict)
+    except:
+        abort(404)
 
 
 if __name__ == "__main__":
